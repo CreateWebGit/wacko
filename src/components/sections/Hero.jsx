2dc57886e40ae07abb72bd2a7e9def34f5dc6e87
emilavara <emilavara@hotmail.com>
2025-06-09 06:56:32 +0000

    changes year on hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,23 +1,33 @@
 import Link from 'next/link'
 
 export default function SectionHero() {
-    return (
-        <section className="cw-section--hero cw-grid full-width">
-            <div className="cw-col-6 cw-col-xs-12 cw-c-justify-center cw-flex-column pl-4">
-                <h1 className="text-white text-center-xs">Tidlös elegans <br></br> i äkta skinn.</h1>
-                <p className="mt-2 text-white text-center-xs">
-                    Sedan 1992 har vi klätt Stockholm i skinn med attityd – där tidlös elegans möter <br className="hide-mobile"></br> modern edge. Välkommen till våra butiker på Södermalm eller utforska vårt urval <br></br> direkt online.
-                </p>
-                <div className="d-flex gap-1 mt-4 button-container">
-                    {/* <button className="primary">Utforska kollektionen</button>
+  return (
+    <section className="cw-section--hero cw-grid full-width">
+      <div className="cw-col-6 cw-col-xs-12 cw-c-justify-center cw-flex-column pl-4">
+        <h1 className="text-white text-center-xs">
+          Tidlös elegans <br></br> i äkta skinn.
+        </h1>
+        <p className="mt-2 text-white text-center-xs">
+          Sedan 1982 har vi klätt Stockholm i skinn med attityd – där tidlös elegans möter{' '}
+          <br className="hide-mobile"></br> modern edge. Välkommen till våra butiker på Södermalm
+          eller utforska vårt urval <br></br> direkt online.
+        </p>
+        <div className="d-flex gap-1 mt-4 button-container">
+          {/* <button className="primary">Utforska kollektionen</button>
                     <button className="stroked hide-mobile">Hitta din närmaste butik</button> */}
-                    <Link href="/herr" className="button primary">Utforska kollektionen</Link>
-                    <Link href="/#hitta-hit" className="button stroked hide-mobile">Hitta din närmaste butik</Link>
-                    <a className="text-white hide-desktop" href="/#hitta-hit">Hitta din närmaste butik -&gt;</a>
-                </div>
-            </div>
-            <div className="cw-col-6 cw-col-xs-12 empty-col"></div>
-            <video src="/wacko_bg_video.mp4" autoPlay playsInline loop muted></video>
-        </section>
-    )
+          <Link href="/herr" className="button primary">
+            Utforska kollektionen
+          </Link>
+          <Link href="/#hitta-hit" className="button stroked hide-mobile">
+            Hitta din närmaste butik
+          </Link>
+          <a className="text-white hide-desktop" href="/#hitta-hit">
+            Hitta din närmaste butik -&gt;
+          </a>
+        </div>
+      </div>
+      <div className="cw-col-6 cw-col-xs-12 empty-col"></div>
+      <video src="/wacko_bg_video.mp4" autoPlay playsInline loop muted></video>
+    </section>
+  )
 }
